5e37a054cbf8dde71749d98c4e9be46b4a9b8ab3
Enrique Arriaga <arriaga.v.enrique@gmail.com>
2017-11-07 17:14:23 -0600

    finish video 25, add navbar

--- sitio/music/views.py
+++ sitio/music/views.py
@@ -1,16 +1,27 @@
-from django.http import Http404
-from django.shortcuts import render
-from .models import Album
+from django.shortcuts import render, get_object_or_404
+from .models import Album, Song
 
 
 def index(request):
     all_albums = Album.objects.all()
-    context = { 'all_albums': all_albums }
-    return render(request, 'music/index.html', context)
+    return render(request, 'music/index.html', { 'all_albums': all_albums })
+
 
 def detail(request, album_id):
+    album = get_object_or_404(Album, id=album_id)
+    return render(request, 'music/detail.html', { "album": album })
+
+
+def favorite(request, album_id):
+    album = get_object_or_404(Album, id=album_id)
     try:
-        album = Album.objects.filter(id=album_id).get()
-    except Album.DoesNotExist:
-        raise Http404("Album " + album_id + " does not exist")
-    return render(request, 'music/detail.html', { "album": album})
+        selected_song = album.song_set.get(id=request.POST['song'])
+    except (KeyError, Song.DoesNotExist):
+        return render(request, 'music/detail.html', {
+            "album": album,
+            "error_message": "You did not select a valid song"
+        })
+    else:
+        selected_song.is_favorite = True
+        selected_song.save()
+        return render(request, 'music/detail.html', { "album": album })
